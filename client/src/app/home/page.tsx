32fe01bf3152118add75b0a9a659e8a90f8271bf
NotSugat <sugatsujakhu@gmail.com>
2023-09-29 18:25:58 +0545

    auth and video player added

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,11 +1,27 @@
-import React from "react";
-import Navbar from "../components/Navbar";
-import LiveCards from "../components/LiveCards";
+"use client";
+import React, { useEffect, useRef } from "react";
+import Hls from "hls.js";
+
 const HomePage = () => {
+  const videoRef = useRef(null);
+  const videoSrc = "http://localhost:8080/hls/test.m3u8";
+
+  useEffect(() => {
+    const video: any = videoRef.current;
+
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(videoSrc);
+      hls.attachMedia(video);
+    } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+      video.src = videoSrc;
+    }
+  }, []);
+
   return (
     <div>
-      <Navbar />
-      <LiveCards />
+      <h2>Live Stream</h2>
+      <video ref={videoRef} width="800" controls></video>
     </div>
   );
 };
