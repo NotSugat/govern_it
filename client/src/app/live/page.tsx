deefc2b10fb84916013894b3abb76ddeca8177af
Anish <anishtimalsina747@gmail.com>
2023-09-29 20:08:47 +0545

    added live chat

--- client/src/app/live/page.tsx
+++ client/src/app/live/page.tsx
@@ -1,8 +1,12 @@
 "use client";
-import React, {useState} from 'react';
+import React, {useState,useEffect, FormEvent} from 'react';
 import SingleMessage from '../components/SingleMessage';
 import './homepage.css';
 import Navbar from '../components/Navbar';
+import {io,Socket} from 'socket.io-client';
+import { useParams } from 'next/navigation';
+import { useDispatch, useSelector } from "react-redux";
+
 
 interface SingleMessageProp {
   name: string;
@@ -10,14 +14,73 @@ interface SingleMessageProp {
   message: string;
 }
 
-const Live = () => {
-  const [allMessages, setAllMessages] = useState([]);
-  const [inputText, setInputText] = useState('');
-  const now = new Date();
-  const hours = String(now.getHours()).padStart(2, '0');
-  const minutes = String(now.getMinutes()).padStart(2, '0');           
-  const currentTime = `${hours}:${minutes}`
+interface Message {
+    inputText: string;
+    userName: string;
+    currentTime: string;
+  }
+  
 
+const Live = () => {
+
+    const [allMessages, setAllMessages] = useState<Message[]>([]);    ;
+    const [inputText, setInputText] = useState('');
+    const {userInfo} = useSelector((state)=>state.auth);
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');           
+    const currentTime = `${hours}:${minutes}`
+    const {id:stream_id} = useParams();
+    const userName = userInfo?.rows?.[0]?.name ?? "Anonymous User";
+    const socket: Socket = io("http://localhost:3001");
+
+    socket.on("connect", () => {
+        console.log("Connected to Socket.IO server");
+      });
+      
+    socket.on("message", (data: string) => {
+        console.log("Received message from server:", data);
+    });
+    useEffect(()=>{
+        if (socket == null) return; 
+        socket.emit('joinRoomCode',stream_id)
+        return ()=>{
+            socket.off('joinRoomCode');
+        }
+    },[socket, stream_id])
+
+    useEffect(()=>{
+        if(socket == null) return ; 
+        
+        socket.on("message",(receivedMessage)=>{
+            console.log(receivedMessage)
+        });
+        
+        socket.on("receiveChatMessage", (data)=>{
+            let updatedData = {};
+            updatedData = data;
+             setAllMessages((allMessages)=>[
+                ...allMessages, 
+                {inputText: data.inputText, userName: data.userName, currentTime: data.currentTime}
+            ]
+            );
+        });
+        
+        return ()=>{
+            socket.off('message')
+        }
+    },[socket])
+
+    const submitForm = (e: FormEvent)=>{
+        e.preventDefault();
+        if(socket == null) return ;
+
+        const currentTime = `${hours}:${minutes}`
+
+        socket.emit('chatMessage',{inputText, userName, currentTime})
+        setInputText('')
+    } 
+      
 
   return (
     <>
@@ -40,7 +103,7 @@ const Live = () => {
                     </div>
                 </main>
                 <div className="chat-form-container">
-                    <form id="chat-form">
+                    <form id="chat-form" onSubmit={submitForm}>
                         <input
                             id="msg"
                             value={inputText}
---
+++ server/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import { Server, Socket } from 'socket.io';
+
+const app = express();
+const io = new Server(3001,{
+    cors:{
+        origin: 'http://localhost:3000',
+        methods: ["GET", "POST"],
+    }
+});
+
+io.on("connection", (socket) => {
+  console.log("SOCKET CONNECTED")
+  socket.on('joinRoomCode',(roomCode)=>{
+    socket.join(roomCode);
+
+    // Send a welcome message to the user who joined
+    socket.emit('message', 'Welcome to the room');
+
+    // Send a message to all members in the room (including the sender)
+    io.to(roomCode).emit('message', 'A user has joined the room');
+    
+    // Listen for chat messages
+    socket.on('chatMessage',(data)=>{
+      io.to(roomCode).emit("receiveChatMessage", data);
+    });
+
+    // Message on disconnection
+    socket.on('disconnect',()=>{
+      io.to(roomCode).emit('message','A user has left the room');
+    });
+  });
+});
+
+io.listen(3000);
+
+app.listen(5000, () => {
+  // console.log('Server listening on port 3001');
+});
