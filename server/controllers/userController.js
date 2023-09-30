64237299366ffabce8c1a67f95edd88910dd5ea5
Anish <anishtimalsina747@gmail.com>
2023-10-01 00:09:20 +0545

    chat added to live

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,87 +6,61 @@ import { uuid } from "uuidv4";
 
 
 const registerUser = expressAsyncHandler(async (req, res, next) => {
-    let { email, name, password } = req.body;
+    const { email, name, password } = req.body;
+
     try {
         const dbRef = ref(getDatabase());
-        get(child(dbRef, `users/${name}`)).then((snapshot) => {
-            if (snapshot.exists()) {
-                console.log(snapshot.val());
-            } else {
-                console.log("No data available");
-            }
-        }).catch((error) => {
-            console.error(error);
-        });
-        res.json("Hello")
-
+        const snapshot = await get(child(dbRef, `users/${name}`));
+    
+        if (snapshot.exists()) {
+            console.log("User with the same name already exists");
+            res.status(400).json({ error: 'User with the same name already exists' });
+        } else {
+            const salt = await bcrypt.genSalt(5);
+            const hashedPassword = await bcrypt.hash(password, salt);
+    
+            await set(child(dbRef, `users/${name}`), {
+                email: email,
+                password: hashedPassword // Store the hashed password, not the plain password
+            });
+    
+            res.status(200).json("User inserted successfully");
+        }
     } catch (error) {
         console.error('Error registering user:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
+    
 });
 
 
 //public route
 const authUser = expressAsyncHandler(async (req, res) => {
     const { name, password } = req.body;
-
     try {
         const userRef = ref(database, 'users/' + name);
-        onValue(userRef, async (snapshot) => {
-            const data = await snapshot.val();
-            //   console.log(data.password)
-            if (snapshot.exists()) {
-                if (await bcrypt.compare(password, data.password)) {
-                    // generateToken(res,checkUser.rows[0].id)
-                    res.status(201)
-                    res.json(data)
-                    return;
-                } else {
-                    res.status(401)
-                    res.send("Error")
-                    return;
-                    // throw new Error("Invalid Email or Password")
-                }
+        const snapshot = await get(userRef);
+    
+        if (snapshot.exists()) {
+            const data = snapshot.val();
+            const passwordMatch = await bcrypt.compare(password, data.password);
+    
+            if (passwordMatch) {
+                // Password matches, send success response with user data
+                res.status(200).json(data);
             } else {
-                res.status(400).json("Invalid Email or Password");
-                return;
+                // Password does not match, send unauthorized error response
+                res.status(401).json({ error: 'Invalid Password' });
             }
-        });
+        } else {
+            // User does not exist, send not found error response
+            res.status(404).json({ error: 'User Not Found' });
+        }
     } catch (error) {
         console.error('Error Signing user:', error);
         res.status(500).json({ error: 'Internal Server Error' });
-    }
+    }    
 });
 
 
-// const logOutUser = expressAsyncHandler(async(req,res) =>{
-//     res.cookie('jwt','',{
-//         httpOnly: true,
-//         expires: new Date(0)
-//     })
-
-//     res.status(200).json({message: "Logged Out Successfully"})
-// });
-
-// const getUserProfile = expressAsyncHandler(async(req,res) =>{
-//     console.log(req.user)
-//     res.status(200).json({mesage:"getUserProfile"})
-
-// });
-
-// const updateProfile = expressAsyncHandler(async(req,res) =>{
-//     const {name} = req.body;
-//     const id = req.user.rows[0].id;
-//     const checkUser = await pool.query("SELECT * FROM users WHERE id = $1",[id])
-//     if (checkUser.rowCount !== 0) {
-//         const updateDetails = await pool.query("UPDATE users SET name=$1 WHERE id=$2",[name,id])            
-//         res.status(200)
-//         res.json(updateDetails)
-//     }else{
-//         res.status(404)
-//         throw new Error('User Not Found')
-//     }
-// });
-// export  {authUser,registerUser,logOutUser,getUserProfile,updateProfile};
 export { registerUser, authUser }
