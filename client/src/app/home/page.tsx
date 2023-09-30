a1ee2c47da897665224ec3c7d9b3bd4498b9861d
NotSugat <sugatsujakhu@gmail.com>
2023-10-01 02:26:46 +0545

    assets added

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,18 +1,20 @@
 // "use client"
 import React from "react";
 import LiveCards from "../components/LiveCards";
-import Navbar from '../components/Navbar';
-import { useSelector } from 'react-redux';
-import {store, useAppSelector, useAppDispatch} from '../../redux/store'
+import Navbar from "../components/Navbar";
+import { useSelector } from "react-redux";
+import { store, useAppSelector, useAppDispatch } from "../../redux/store";
 
 const HomePage = () => {
-  console.log("home")
+  console.log("home");
   const isEnglish = store.getState().isEnglish;
-  console.log(isEnglish)
-  return <div>
-    <Navbar />
-    <LiveCards />
-  </div>;
+  console.log(isEnglish);
+  return (
+    <div>
+      <Navbar />
+      <LiveCards />
+    </div>
+  );
 };
 
 export default HomePage;
